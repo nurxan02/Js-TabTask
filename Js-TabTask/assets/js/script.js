14f2ef1e95e0859b58d9fd141ae21612ce7d1d2d
Nurxan <90649844+nurxan02@users.noreply.github.com>
2021-11-24 05:49:07 +0400

    ver 1

---
+++ Js-TabTask/assets/js/script.js
@@ -0,0 +1,15 @@
+let button = document.querySelector(".btn")
+button.onclick = function () {
+    button.style.backgroundColor = "rgb(69, 161, 160)"
+    
+}
+// let lastbox;
+
+// button.forEach(btn=>{
+//     btn.onclick = function () {
+//      lastbox ? lastbox.style.backgroundColor = "rgb(173, 173, 173)" : null;
+//      lastbox = tab
+//      box.style.backgroundColor = rgb(69, 161, 160);
+
+//     }
+// })
